a7c8c601220ebc74503fceeeb6d373be4c82dcef
Nagesh Kekan <nagesh2086@gmail.com>
2018-10-18 19:23:16 +0530

    Strategy pattern example and info

--- org/me/design/pattern/package-info.java
+++ org/me/design/pattern/package-info.java
@@ -1,5 +1,8 @@
 /**
+ * Command pattern - Struts Action classes and their associated action mappings are like the Command pattern.
  * 
+ * Strategy\policy pattern - Collections.sort\Arrays.sort and comparator. 
+ * Useful when we want to select runtime algorithm\solution depending on input.
  */
 /**
  * @author nagesh2086
